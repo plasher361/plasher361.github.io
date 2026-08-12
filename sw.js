1f155f812c6c3a5a1ac4d04ba89321d747e6227e
plasher361 <merttopal30@gmail.com>
2026-08-12 22:03:33 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Ardeşen Karate Spor Kulübü — Service Worker
    Uygulamayı önbelleğe alır, internet olmadan da açılmasını sağlar. */
 
-const CACHE = 'ardesen-karate-v3';
+const CACHE = 'ardesen-karate-v4';
 
 const DOSYALAR = [
   './',
